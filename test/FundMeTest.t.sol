dc218268f4849a5f0863e18f19bf4e423bc29c41
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-16 12:08:16 -0500

    feat: test to validate ownership

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -9,10 +9,18 @@ contract FundMeTest is Test {
 
     // setUp always runs first
     function setUp() external {
+        // us -> FundMeTest -> FundMe
         fundMe = new FundMe();
     }
 
     function testMinimumDollarIsFive() public {
        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        console.log(fundMe.i_owner());
+        console.log(msg.sender);
+        // assertEq(fundMe.i_owner(), msg.sender);  // -> This will not work because the sender is different that the address which deploy the contract
+        assertEq(fundMe.i_owner(), address(this)); // -> This will work because the sender is the same that the address which deploy the contract (address(this))
     }
 }
