fef8cc6a5e5dca2f8c4596b2e09d915f05e4364c
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-16 12:52:09 -0500

    feat: updating address(this) to msg.sender

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -24,7 +24,7 @@ contract FundMeTest is Test {
         console.log(fundMe.i_owner());
         console.log(msg.sender);
         // assertEq(fundMe.i_owner(), msg.sender);  // -> This will not work because the sender is different that the address which deploy the contract
-        assertEq(fundMe.i_owner(), address(this)); // -> This will work because the sender is the same that the address which deploy the contract (address(this))
+        assertEq(fundMe.i_owner(), msg.sender); // -> This will work because the sender is the same that the address which deploy the contract (address(this))
     }
 
     // What can we do to work with addresses outside our system?
