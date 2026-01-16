f701750e0f65429e56bfe0512db835ab69443d4b
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-16 12:28:49 -0500

    feat: adding notes of test's types

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -23,4 +23,19 @@ contract FundMeTest is Test {
         // assertEq(fundMe.i_owner(), msg.sender);  // -> This will not work because the sender is different that the address which deploy the contract
         assertEq(fundMe.i_owner(), address(this)); // -> This will work because the sender is the same that the address which deploy the contract (address(this))
     }
+
+    // What can we do to work with addresses outside our system?
+    // 1. Unit
+    //    - Testing a specific part of our code
+    // 2. Integration
+    //    - Testing how our code works with other parts of our code
+    // 3. Forked
+    //    - Testing our code on a simulated real environment
+    // 4. Staging
+    //    - Testing our code on a real environment that is not prod
+
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4); // This will give us an error because when we test a contract without specifying the network, anvil put some address
+    }
 }
