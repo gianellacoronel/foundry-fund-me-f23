b7ab87b16c4803858d67786e7ab89c12c86006a9
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-16 11:52:07 -0500

    feat: update error name

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -36,7 +36,7 @@ contract FundMe {
 
     modifier onlyOwner() {
         // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
         _;
     }
 
