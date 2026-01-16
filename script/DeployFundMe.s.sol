3dc99245dcd184e59212083ef78c18b62026e654
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-16 13:08:36 -0500

    feat: deploy mock priceFeed

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -4,11 +4,18 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
 
 contract DeployFundMe is Script{
     function run() external returns (FundMe){
+        // Before startBroadcast -> Not a "real" tx (transaction)
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
+        // After startBroadcast -> "real" tx (transaction)
         vm.startBroadcast();
-        new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // Mock
+        new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
