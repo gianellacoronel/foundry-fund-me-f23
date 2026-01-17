6770014da418826a13c4181e07934efb1c40b7b6
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-17 12:52:57 -0500

    feat: add testAddsFunderToArrayOfFunders

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -65,4 +65,12 @@ contract FundMeTest is Test {
         uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
         assertEq(amountFunded, SEND_VALUE);
     }
+
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
 }
