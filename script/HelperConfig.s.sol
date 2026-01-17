95fdd7e7c13c2b9698f9070ebc19f72c6f887f87
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2026-01-17 12:14:01 -0500

    feat: use constants for magical numbers

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -13,6 +13,9 @@ contract HelperConfig is Script{
     // Otherwise, grab the existing address from the live network
     NetworkConfig public activeNetworkConfig;
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
     struct NetworkConfig {
        address priceFeed; // ETH/USD price feed address
     }
@@ -34,12 +37,13 @@ contract HelperConfig is Script{
     }
     //Like we are using vm, we can't have this function pure
     function getAnvilEthConfig() public returns (NetworkConfig memory) {
+
         // price feed address
         // 1. Deploy the mocks
         // 2. Return the mock address
 
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
         vm.stopBroadcast();
 
         NetworkConfig memory anvilConfig = NetworkConfig({
